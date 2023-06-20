c014cb3715dd8c52fa299029f2b4a35ef81e460a
Jibrand <137154043+Jibrand@users.noreply.github.com>
2023-06-20 03:36:36 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/Jibran220"
+                href="https://github.com/jibrand"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
