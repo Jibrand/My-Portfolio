7350a5103f36ad185d4936b5c394a87779f3bd6b
Jibrand <muhammadjibrandev@gmail.com>
2023-10-22 11:59:53 -0700

    ds

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -22,6 +22,7 @@ import as89 from '../../Assets/po.jpg'
 import as9 from '../../Assets/p7.png'
 import as10 from '../../Assets/p11.png'
 import as11 from '../../Assets/p13.png'
+import as12 from '../../Assets/A.PNG'
 
 function Projects() {
   return (
@@ -35,12 +36,15 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+
+         
+
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={cont}
               isBlog={false}
               title="Open AI Application (Copymatic) | Frontend (MERN)"
-              description="I design the frontend of Open AI application to generate the Automatic blog and content, such as Copymatic and Chat Gpt, use Tailwind Css with glass morphism effects, Mobile Responsive, and eye-catchy."
+              description="Designed the frontend of Open AI application to generate the Automatic blog and content, such as Copymatic and Chat Gpt, use Tailwind Css with glass morphism effects, Mobile Responsive, and eye-catchy."
               ghLink="https://github.com/Jibrand"
               demoLink="https://dribbble.com/shots/22527970-Open-AI-Application-Copymatic-Frontend-MERN"
             />
@@ -51,9 +55,9 @@ function Projects() {
               imgPath={saas}
               isBlog={false}
               title=" Project Management, SAAS Application | Full Stack (MERN)"
-              description="I design both frontend and backend using cloud database (mongodb), RFQ will generate whichwill be invited to vendors to get their Quotes, different profile section for them and for the higher-Committee, the Data is Authenticated and Safe."
-               ghLink="https://github.com/Jibrand/SAAS-Bussiness-Application-FRONTEND"
-              //  demoLink="http://54.37.6.229:3000/"
+              description="Designed both frontend and backend using cloud database (mongodb), RFQ will generate whichwill be invited to vendors to get their Quotes, different profile section for them and for the higher-Committee, the Data is Authenticated and Safe."
+              ghLink="https://github.com/Jibrand/SAAS-Bussiness-Application-FRONTEND"
+            //  demoLink="http://54.37.6.229:3000/"
             />
           </Col>
 
@@ -73,7 +77,7 @@ function Projects() {
               imgPath={as11}
               isBlog={false}
               title="Digital Marketing Agency | Next.js"
-              description="Recently, I completed the development of a sophisticated website for our digital marketing agency, showcasing our expertise and services"
+              description="Recently, Completed the development of a sophisticated website for our digital marketing agency, showcasing our expertise and services"
               ghLink="https://github.com/Jibrand/My-Agency"
               demoLink="https://webtose.netlify.com/"
             />
@@ -84,9 +88,19 @@ function Projects() {
               imgPath={as}
               isBlog={false}
               title="Job Finding | Mobile App (Cross Platform) | React Native"
-              description="I made the Job Finding app in React Native. Jobs are listed from Rapid API, Users can selectand apply for jobs by joining their respective websites from the app."
+              description="Made the Job Finding app in React Native. Jobs are listed from Rapid API, Users can selectand apply for jobs by joining their respective websites from the app."
               ghLink="https://github.com/Jibrand/React-Native-Jobs-Application"
-              demoLink="https://dribbble.com/shots/22528015-Job-Finding-Mobile-App-Cross-Platform-React-Native"              
+              demoLink="https://dribbble.com/shots/22528015-Job-Finding-Mobile-App-Cross-Platform-React-Native"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={as12}
+              isBlog={false}
+              title="Bee Hive Investment Project | MERN Stack | Sanity"
+              description="Developed the Bee Hive Investment Project, allowing investors to purchase hive trees and other assets. After 2-3 months, investors receive revenue generated from honey, pollen, and other products obtained from their hive. Integrated Stripe for secure transactions and utilized Sanity for efficient content management and organization."
+              demoLink="https://beequeencoin.com/"
             />
           </Col>
 
@@ -95,18 +109,20 @@ function Projects() {
               imgPath={as2}
               isBlog={false}
               title="Blockchain Cryptocurrency App | Web 3.0"
-              description="I made the Blockchain (Web 3.0) application through which we can send Ethereum to any Blockchain Address and charge the Gas Price, against each transaction, also record all of the transactions with a unique gif."
+              description="Made the Blockchain (Web 3.0) application through which we can send Ethereum to any Blockchain Address and charge the Gas Price, against each transaction, also record all of the transactions with a unique gif."
               ghLink="https://github.com/Jibrand/Web-3.0"
               demoLink="https://mykryptc.netlify.app/"
             />
           </Col>
+
+          
 
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={as4}
               isBlog={false}
               title="Shooinndmv |  Best Sneaker Seller [WordPress]"
-              description=" It is the Ecommerce website of Sneaker, Which offer high-quality sneakers and shoes, including the latest trends and timeless classics, to meet your footwear needs."
+              description=" It is an Ecommerce website of Sneaker, Which offer high-quality sneakers and shoes, including the latest trends and timeless classics, to meet your footwear needs."
               ghLink="https://github.com/Jibrand/"
               demoLink="https://shooinndmv.com/"
             />
@@ -117,7 +133,7 @@ function Projects() {
               imgPath={as5}
               isBlog={false}
               title="Ecommerce Website |  Asp.Net MVC"
-              description="I've developed an ASP.NET and MS SQL Server e-commerce website for furniture, featuring comprehensive CRUD operations for seamless management."
+              description="Developed an ASP.NET and MS SQL Server e-commerce website for furniture, featuring comprehensive CRUD operations for seamless management."
               ghLink="https://github.com/Jibrand/Online-Shop-Asp.Net"
               demoLink="https://www.youtube.com/watch?v=Ucc3mgK4mFc&t=7s"
             />
@@ -128,7 +144,7 @@ function Projects() {
               imgPath={as6}
               isBlog={false}
               title="Pavilion Mart | Desktop Application [C#]"
-              description="I've created a C# desktop application for a supermarket, functioning as a shopping cart and billing software, utilizing MS SQL Server for data management."
+              description="Created a C# desktop application for a supermarket, functioning as a shopping cart and billing software, utilizing MS SQL Server for data management."
               ghLink="https://github.com/Jibrand/Pavilion-Mart-CSharp"
               demoLink="https://www.youtube.com/watch?v=69lIBl2GBmE&t=26s"
             />
@@ -139,7 +155,7 @@ function Projects() {
               imgPath={as7}
               isBlog={false}
               title="Point Of Sale | Desktop Application [C#]"
-              description="I've created a C# desktop application for The Business to Store their Ledger and Stock, functioning as a Point Of Sale Software, utilizing MS SQL Server for data management."
+              description="Created a C# desktop application for The Business to Store their Ledger and Stock, functioning as a Point Of Sale Software, utilizing MS SQL Server for data management."
               ghLink="https://github.com/Jibrand"
               demoLink="https://www.youtube.com/watch?v=Ki4zuaZgKWY"
             />
@@ -150,9 +166,9 @@ function Projects() {
               imgPath={as8}
               isBlog={false}
               title="Hotel Management Software | Desktop Application [C#]"
-              description="I've created a C# desktop application for The Business to Store their Hotal Data and The Customer Data, functioning as a Management Software."
+              description="Created a C# desktop application for The Business to Store their Hotal Data and The Customer Data, functioning as a Management Software."
               ghLink="https://github.com/Jibrand"
-              demoLink="https://www.youtube.com/watch?v=Ki4zuaZgKWY"
+              demoLink="https://www.youtube.com/watch?v=OdeCsFZ0Q8k"
             />
           </Col>
 
@@ -161,7 +177,7 @@ function Projects() {
               imgPath={as89}
               isBlog={false}
               title="Bookeeping Accounting Website | Manage the Accounts [WordPress]"
-              description="I've successfully created a comprehensive bookkeeping and accounting website, simplifying financial management for businesses and individuals alike."
+              description="Successfully created a comprehensive bookkeeping and accounting website, simplifying financial management for businesses and individuals alike."
               ghLink="https://github.com/Jibrand"
               demoLink="https://superiorsums.com/"
             />
@@ -172,22 +188,21 @@ function Projects() {
               imgPath={as9}
               isBlog={false}
               title="Education Management Software | Web Application [HTML, CSS, JQuery]"
-              description="I've created a Web application for The Education for admission and tracking all the data of students, functioning as a Education Management Software."
+              description="Created a Web application for The Education for admission and tracking all the data of students, functioning as a Education Management Software."
               ghLink="https://github.com/Jibrand"
-              demoLink="https://www.youtube.com/watch?v=Ki4zuaZgKWY"
+              demoLink="https://www.youtube.com/watch?v=PZAE9wPbGYQ&t=218s"
             />
           </Col>
 
-          
+
 
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={as3}
               isBlog={false}
               title="Tons of Web & desktop Applications"
-              description="I built tons of websites with the passage of time, with Different Programming Languages and With other Platforms such as using MERN Stack, WordPress, Wix, and Shopify, interacting with many databases such as Mongo Db, Firebase, Ms. SQL Server, My SQL, etc."
-              ghLink="https://github.com/jibran220"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              description="Built tons of websites with the passage of time, with Different Programming Languages and With other Platforms such as using MERN Stack, WordPress, Wix, and Shopify, interacting with many databases such as Mongo Db, Firebase, Ms. SQL Server, My SQL, etc."
+              ghLink="https://github.com/jibrand"
             />
           </Col>
         </Row>
@@ -197,4 +212,3 @@ function Projects() {
 }
 
 export default Projects;
-  
